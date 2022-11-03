fd536415f726df770f1aeffbe73fd4a76bdb0333
Dmitry Tsybulenko <45.vilka@mail.ru>
2022-11-03 20:04:31 +0300

    Go

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+SORTING = 10
+
 
 def index(requests):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:SORTING]
     context = {
         'posts': posts,
     }
     return render(requests, 'posts/index.html', context)
-
-
-SORTING = 10
 
 
 def group_posts(request, slug):
